8be0a745247d744d31890b5b1662be214783ba28
nitipong s <s.nitipong@gmail.com>
2025-08-22 10:41:16 +0700

    update

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -79,7 +79,7 @@ const Footer = () => {
           </nav>
           <nav>
             <h6 className="footer-title">Quick Link</h6>
-            <a href="/landing/catalog" className="link link-hover">About us</a>
+            <a href="/landing/about" className="link link-hover">About us</a>
             <a href="/landing/contact" className="link link-hover">Contact</a>
           </nav>
           <nav>
